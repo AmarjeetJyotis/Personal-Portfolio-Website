665dd46f3402f9a8292d49e82f59b43bb59f0ce9
AMARJEET KUMAR <140300391+AmarjeetJyotis@users.noreply.github.com>
2025-07-02 12:15:40 +0530

    Update footer.jsx

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -17,7 +17,7 @@ function Footer() {
           <div className="flex items-center gap-5">
             <Link
               target="_blank"
-              href="https://github.com/AmarjeetJyotis/developer-portfolio.git"
+              href="https://github.com/AmarjeetJyotis/Personal-Portfolio-Website"
               className="flex items-center gap-2 uppercase hover:text-[#16f2b3]"
             >
               <IoStar />
@@ -25,7 +25,7 @@ function Footer() {
             </Link>
             <Link
               target="_blank"
-              href="https://github.com/AmarjeetJyotis/developer-portfolio/fork"
+              href="https://github.com/AmarjeetJyotis/Personal-Portfolio-Website/fork"
               className="flex items-center gap-2 uppercase hover:text-[#16f2b3]"
             >
               <CgGitFork />
